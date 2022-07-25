cb13ac0f5fc98b48d1ff23e8b03297656f444ee8
Wycliffe-Kim <f16o@hanyang.ac.kr>
2022-07-25 09:38:25 +0900

    correct node

--- algorithm_study/node.py
+++ algorithm_study/node.py
@@ -4,10 +4,6 @@ T = TypeVar('T')
 
 
 class Node(Generic[T]):
-    data: T
-    left: Node[T] | None
-    right: Node[T] | None
-
     def __init__(self, item: T):
         self.data = item
         self.left = None
@@ -19,6 +15,6 @@ class Node(Generic[T]):
         return left_size + right_size + 1
 
     def depth(self):
-        left_depth = self.left.depth() if self.left is not None else 0
-        right_depth = self.right.depth() if self.left is not None else 0
+        left_depth = self.left.depth() if self.left is not None else 1
+        right_depth = self.right.depth() if self.left is not None else 1
         return (left_depth if left_depth > right_depth else right_depth) + 1
