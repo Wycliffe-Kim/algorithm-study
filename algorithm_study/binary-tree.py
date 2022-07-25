07d4b1c535d3cfd4072ca20f279c3e5e0c157676
Wycliffe-Kim <f16o@hanyang.ac.kr>
2022-07-25 09:20:08 +0900

    create depth function

--- algorithm_study/binary-tree.py
+++ algorithm_study/binary-tree.py
@@ -1,4 +1,4 @@
-from typing import Generic, TypeVar
+from typing import Callable, Generic, TypeVar
 from algorithm_study.node import Node
 
 T = TypeVar('T')
@@ -11,4 +11,10 @@ class BinaryTree(Generic[T]):
         self.__root = root
 
     def size(self):
-        return self.__root.size() if self.__root is not None else 0
+        return self.__invoke_with_condition(self.__root.size, 0)
+
+    def depth(self):
+        return self.__invoke_with_condition(self.__root.depth, 0)
+
+    def __invoke_with_condition(self, func: Callable[[], int], default_value: int):
+        return func() if self.__rooot is not None else default_value
--- algorithm_study/node.py
+++ algorithm_study/node.py
@@ -17,3 +17,8 @@ class Node(Generic[T]):
         left_size = self.left.size() if self.left is not None else 0
         right_size = self.right.size() if self.right is not None else 0
         return left_size + right_size + 1
+
+    def depth(self):
+        left_depth = self.left.depth() if self.left is not None else 0
+        right_depth = self.right.depth() if self.left is not None else 0
+        return (left_depth if left_depth > right_depth else right_depth) + 1
