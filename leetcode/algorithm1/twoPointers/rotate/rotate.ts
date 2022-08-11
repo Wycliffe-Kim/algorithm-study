9b088437c231a44171ea37e852c6a019c5aa9dc2
Wycliffe-Kim <f16o@hanyang.ac.kr>
2022-08-11 09:32:37 +0900

    leetcode algorithm1 twoPointers rotate

--- leetcode/algorithm1/twoPointers/rotate/rotate.ts
+++ leetcode/algorithm1/twoPointers/rotate/rotate.ts
@@ -2,7 +2,6 @@ const reverse = (nums: number[], start: number, end: number) => {
   let temp: number = nums[0];
   const size = end - start;
 
-  console.log(size);
   if (size > 0) {
     for (let i = 0; i < Math.floor(size / 2); i++) {
       const point1 = i + start;
