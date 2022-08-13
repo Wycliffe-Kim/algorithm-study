a207fa2356182abcaefa4a5490f30eefdfb9c00a
Wycliffe-Kim <f16o@hanyang.ac.kr>
2022-08-13 14:51:02 +0900

    leetcode algorithm1 twoPointers removeNthFromEnd

---
+++ leetcode/algorithm1/twoPointers/removeNthFromEnd/removeNthFromEnd.test.ts
@@ -0,0 +1,39 @@
+import { ListNode } from '../../../datastructure1/linkedList/ListNode';
+import { pipe2 } from '../../../functions/pipe';
+import { removeNthFromEnd } from './removeNthFromEnd';
+
+test('head = [1,2,3,4,5], n = 2', () => {
+  const head = ListNode.fromList([1, 2, 3, 4, 5]);
+  const n = 2;
+  const node = removeNthFromEnd(head, n);
+  const remove = pipe2(ListNode.toList, JSON.stringify);
+  const result = remove(node);
+  expect(result).toBe('[1,2,3,5]');
+});
+
+test('head = [1], n = 1', () => {
+  const head = ListNode.fromList([1]);
+  const n = 1;
+  const node = removeNthFromEnd(head, n);
+  const remove = pipe2(ListNode.toList, JSON.stringify);
+  const result = remove(node);
+  expect(result).toBe('[]');
+});
+
+test('head = [1,2], n = 1', () => {
+  const head = ListNode.fromList([1, 2]);
+  const n = 1;
+  const node = removeNthFromEnd(head, n);
+  const remove = pipe2(ListNode.toList, JSON.stringify);
+  const result = remove(node);
+  expect(result).toBe('[1]');
+});
+
+test('head = [1,2], n = 2', () => {
+  const head = ListNode.fromList([1, 2]);
+  const n = 2;
+  const node = removeNthFromEnd(head, n);
+  const remove = pipe2(ListNode.toList, JSON.stringify);
+  const result = remove(node);
+  expect(result).toBe('[2]');
+});
---
+++ leetcode/algorithm1/twoPointers/removeNthFromEnd/removeNthFromEnd.ts
@@ -0,0 +1,38 @@
+import { ListNode } from '../../../datastructure1/linkedList/ListNode';
+
+export function removeNthFromEnd(
+  head: ListNode | null,
+  n: number
+): ListNode | null {
+  if (head && !head.next) {
+    return null;
+  }
+
+  let first: ListNode | null = head;
+  let second: ListNode | null = head;
+
+  for (let i = 0; i < n; i++) {
+    first = first ? first.next : null;
+  }
+
+  if (!first && head) {
+    return head.next;
+  }
+
+  let prev: ListNode | null = null;
+  while (first) {
+    if (!first.next) {
+      prev = second;
+    }
+    first = first.next;
+    if (second) {
+      second = second.next;
+    }
+  }
+
+  if (prev && second) {
+    prev.next = second.next;
+  }
+
+  return head;
+}
