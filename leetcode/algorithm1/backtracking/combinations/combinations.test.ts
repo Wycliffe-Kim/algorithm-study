2fd78be206e35a21b5df44face3ceb3c527ff347
Wycliffe-Kim <f16o@hanyang.ac.kr>
2022-08-27 16:54:05 +0900

    leetcode algorithm1 backtracking combinations

--- leetcode/algorithm1/backtracking/combinations/combinations.test.ts
+++ leetcode/algorithm1/backtracking/combinations/combinations.test.ts
@@ -1,11 +1,18 @@
-import { combine } from './combinations';
+import { combination } from './combinations';
+
+test('n = 1, k = 1', () => {
+  const result = combination(1, 1);
+  expect(JSON.stringify(result)).toBe('[[1]]');
+});
 
 test('n = 4, k = 2', () => {
-  const result = combine(4, 2);
+  const result = combination(4, 2);
   expect(JSON.stringify(result)).toBe('[[1,2],[1,3],[1,4],[2,3],[2,4],[3,4]]');
 });
 
-test('n = 1, k = 1', () => {
-  const result = combine(1, 1);
-  expect(JSON.stringify(result)).toBe('[[1]]');
+test('n = 5, k = 3', () => {
+  const result = combination(5, 3);
+  expect(JSON.stringify(result)).toBe(
+    '[[1,2,3],[1,2,4],[1,2,5],[1,3,4],[1,3,5],[1,4,5],[2,3,4],[2,3,5],[2,4,5],[3,4,5]]'
+  );
 });
--- leetcode/algorithm1/backtracking/combinations/combinations.ts
+++ leetcode/algorithm1/backtracking/combinations/combinations.ts
@@ -1,8 +1,36 @@
-const factorial = (n: number) => (n > 0 ? n * factorial(n - 1) : 1);
+const _combination = (
+  result: number[][],
+  stack: number[],
+  n: number,
+  k: number,
+  cnt: number
+) => {
+  if (stack.length === k) {
+    result.push([...stack]);
+    console.log(`[condition 1] cnt: ${cnt}, stack:`, stack);
+    return;
+  }
 
-export function combine(n: number, k: number): number[][] {
-  const loop = factorial(n) / Math.pow(factorial(k), 2);
-  const result = Array.from(Array(loop), (): number[] => Array(k).fill(0));
+  if (cnt > n) {
+    console.log('[condition 2]');
+    return;
+  }
 
+  stack.push(cnt);
+  console.log(`[1] cnt: ${cnt},`, 'stack:', stack);
+  _combination(result, stack, n, k, cnt + 1);
+  console.log(`[2] cnt: ${cnt},`, 'stack:', stack);
+  stack.pop();
+  console.log(`[3] cnt: ${cnt},`, 'stack:', stack);
+  _combination(result, stack, n, k, cnt + 1);
+  console.log(`[4] cnt: ${cnt},`, 'stack:', stack);
+};
+
+export function combination(n: number, k: number): number[][] {
+  console.log('----- combination -----', `n: ${n}, k: ${k}`);
+  let result: number[][] = [];
+  const stack: number[] = [];
+  _combination(result, stack, n, k, 1);
+  console.log('result', result);
   return result;
 }
