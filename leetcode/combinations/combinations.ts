eed6e1e071b084441c74364688d7518ef354ec1b
Wycliffe-Kim <f16o@hanyang.ac.kr>
2022-10-18 06:49:37 +0900

    combinations

--- leetcode/combinations/combinations.ts
+++ leetcode/combinations/combinations.ts
@@ -7,30 +7,30 @@ const _combination = (
 ) => {
   if (stack.length === k) {
     result.push([...stack]);
-    console.log(`[condition 1] cnt: ${cnt}, stack:`, stack);
+    // console.log(`[condition 1] cnt: ${cnt}, stack:`, stack);
     return;
   }
 
   if (cnt > n) {
-    console.log('[condition 2]');
+    // console.log('[condition 2]');
     return;
   }
 
   stack.push(cnt);
-  console.log(`[1] cnt: ${cnt},`, 'stack:', stack);
+  // console.log(`[1] cnt: ${cnt},`, 'stack:', stack);
   _combination(result, stack, n, k, cnt + 1);
-  console.log(`[2] cnt: ${cnt},`, 'stack:', stack);
+  // console.log(`[2] cnt: ${cnt},`, 'stack:', stack);
   stack.pop();
-  console.log(`[3] cnt: ${cnt},`, 'stack:', stack);
+  // console.log(`[3] cnt: ${cnt},`, 'stack:', stack);
   _combination(result, stack, n, k, cnt + 1);
-  console.log(`[4] cnt: ${cnt},`, 'stack:', stack);
+  // console.log(`[4] cnt: ${cnt},`, 'stack:', stack);
 };
 
 export function combination(n: number, k: number): number[][] {
-  console.log('----- combination -----', `n: ${n}, k: ${k}`);
+  // console.log('----- combination -----', `n: ${n}, k: ${k}`);
   let result: number[][] = [];
   const stack: number[] = [];
   _combination(result, stack, n, k, 1);
-  console.log('result', result);
+  // console.log('result', result);
   return result;
 }
