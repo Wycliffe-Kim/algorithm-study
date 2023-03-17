ad557bab703166baec9e21f73e8b377e92f4ee98
Wycliffe-Kim <f16o@hanyang.ac.kr>
2023-03-17 14:12:16 +0900

    pytest-watch 추가

--- python/python/oneTwoThree.py
+++ python/python/oneTwoThree.py
@@ -1,5 +1,5 @@
 def oneTwoThree(n):
-    dp = [0, 1, 2, 4, 0, 0, 0, 0, 0, 0, 0, 0]
+    dp = [0, 1, 2, 4, 0, 0, 0, 0, 0, 0, 0]
 
     for i in range(4, n + 1):
         dp[i] = dp[i - 1] + dp[i - 2] + dp[i-3]
--- python/tests/test_oneTwoThree.py
+++ python/tests/test_oneTwoThree.py
@@ -1,16 +1,16 @@
 from oneTwoThree import oneTwoThree
 
 
-def test_oneTwoThree1():
+def test_4to7():
     "4 -> 7"
     assert oneTwoThree(4) == 7
 
 
-def test_oneTwoThree2():
+def test_7to44():
     "7 -> 44"
     assert oneTwoThree(7) == 44
 
 
-def test_oneTwoThree3():
+def test_10to274():
     "10 -> 274"
     assert oneTwoThree(10) == 274
